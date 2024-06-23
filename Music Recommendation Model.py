e1a447dbc3b58bbda204ce3ef6b0194fb6bd3023
Siyona <109352535+siyona1@users.noreply.github.com>
2024-06-24 03:24:37 +0530

    Add files via upload

---
+++ Music Recommendation Model.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+import plotly.express as px 
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.metrics import euclidean_distances
+from scipy.spatial.distance import cdist
+from collections import defaultdict
+import difflib
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+import os
+for dirname, _, filenames in os.walk('data/'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+
+# In[2]:
+
+
+data = pd.read_csv("data/data.csv")
+genre_data = pd.read_csv('data/data_by_genres.csv')
+year_data = pd.read_csv('data/data_by_year.csv')
+artist_data = pd.read_csv('data/data_by_artist.csv')
+
+
+# In[3]:
+
+
+data.head(2)
+
+
+# In[4]:
+
+
+data.info()
+
+
+# In[5]:
+
+
+data['decade'] = data['year'].apply(lambda year : f'{(year//10)*10}s' )
+
+
+# In[6]:
+
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+cluster_pipeline = Pipeline([('scaler', StandardScaler()), ('kmeans', KMeans(n_clusters=12))])
+X = genre_data.select_dtypes(np.number)
+cluster_pipeline.fit(X)
+genre_data['cluster'] = cluster_pipeline.predict(X)
+
+
+# In[7]:
+
+
+from sklearn.manifold import TSNE
+
+tsne_pipeline = Pipeline([('scaler', StandardScaler()), ('tsne', TSNE(n_components=2, verbose=1))])
+genre_embedding = tsne_pipeline.fit_transform(X) # returns np-array of coordinates(x,y) for each record after TSNE.
+projection = pd.DataFrame(columns=['x', 'y'], data=genre_embedding)
+projection['genres'] = genre_data['genres']
+projection['cluster'] = genre_data['cluster']
+
+fig = px.scatter(
+    projection, x='x', y='y', color='cluster', hover_data=['x', 'y', 'genres'],title='Clusters of genres')
+fig.show()
+
+
+# In[8]:
+
+
+song_cluster_pipeline = Pipeline([('scaler', StandardScaler()), 
+                                  ('kmeans', KMeans(n_clusters=25, 
+                                   verbose=False))
+                                 ], verbose=False)
+
+X = data.select_dtypes(np.number)
+song_cluster_pipeline.fit(X)
+song_cluster_labels = song_cluster_pipeline.predict(X)
+data['cluster_label'] = song_cluster_labels
+
+
+# In[9]:
+
+
+from sklearn.decomposition import PCA
+
+pca_pipeline = Pipeline([('scaler', StandardScaler()), ('PCA', PCA(n_components=2))])
+song_embedding = pca_pipeline.fit_transform(X)
+projection = pd.DataFrame(columns=['x', 'y'], data=song_embedding)
+projection['title'] = data['name']
+projection['cluster'] = data['cluster_label']
+
+fig = px.scatter(
+    projection, x='x', y='y', color='cluster', hover_data=['x', 'y', 'title'],title='Clusters of songs')
+fig.show()
+
+
+# In[10]:
+
+
+get_ipython().system('pip install spotipy')
+
+
+# In[11]:
+
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from collections import defaultdict
+CLIENT_ID = "6211dc0e0cad4f319a00a237c11691a9"
+CLIENT_SECRET ="77b3994d08f74dfca4bd09cc3952c37c"
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID, 
+                                                           client_secret=CLIENT_SECRET))
+
+
+# In[12]:
+
+
+def find_song(name, year):
+    song_data = defaultdict()
+    results = sp.search(q= 'track: {} year: {}'.format(name,year), limit=1)
+    if results['tracks']['items'] == []:
+        return None
+
+    results = results['tracks']['items'][0]
+    track_id = results['id']
+    audio_features = sp.audio_features(track_id)[0]
+
+    song_data['name'] = [name]
+    song_data['year'] = [year]
+    song_data['explicit'] = [int(results['explicit'])]
+    song_data['duration_ms'] = [results['duration_ms']]
+    song_data['popularity'] = [results['popularity']]
+
+    for key, value in audio_features.items():
+        song_data[key] = value
+
+    return pd.DataFrame(song_data)
+
+
+# In[13]:
+
+
+number_cols = ['valence', 'year', 'acousticness', 'danceability', 'duration_ms', 'energy', 'explicit',
+ 'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'popularity', 'speechiness', 'tempo']
+
+
+# In[14]:
+
+
+def get_song_data(song, spotify_data):
+    try:
+        song_data = spotify_data[(spotify_data['name'] == song['name']) 
+                                & (spotify_data['year'] == song['year'])].iloc[0]
+        print('Fetching song information from local dataset')
+        return song_data
+    
+    except IndexError:
+        print('Fetching song information from spotify dataset')
+        return find_song(song['name'], song['year'])
+
+
+# In[15]:
+
+
+def get_mean_vector(song_list, spotify_data):
+    song_vectors = []
+    for song in song_list:
+        song_data = get_song_data(song, spotify_data)
+        if song_data is None:
+            print('Warning: {} does not exist in Spotify or in database'.format(song['name']))
+            continue
+        song_vector = song_data[number_cols].values
+        song_vectors.append(song_vector)  
+    
+    song_matrix = np.array(list(song_vectors))#nd-array where n is number of songs in list. It contains all numerical vals of songs in sep list.
+    #print(f'song_matrix {song_matrix}')
+    return np.mean(song_matrix, axis=0)
+
+
+# In[16]:
+
+
+def flatten_dict_list(dict_list):
+    flattened_dict = defaultdict()
+    for key in dict_list[0].keys(): 
+        flattened_dict[key] = [] # 'name', 'year'
+    for dic in dict_list:
+        for key,value in dic.items():
+            flattened_dict[key].append(value) # creating list of values
+    return flattened_dict
+
+
+# In[17]:
+
+
+def recommend_songs( song_list, spotify_data, n_songs=10):
+    
+    metadata_cols = ['name', 'year', 'artists']
+    song_dict = flatten_dict_list(song_list)
+    
+    song_center = get_mean_vector(song_list, spotify_data)
+    #print(f'song_center {song_center}')
+    scaler = song_cluster_pipeline.steps[0][1] # StandardScalar()
+    scaled_data = scaler.transform(spotify_data[number_cols])
+    scaled_song_center = scaler.transform(song_center.reshape(1, -1))
+    distances = cdist(scaled_song_center, scaled_data, 'cosine')
+    #print(f'distances {distances}')
+    index = list(np.argsort(distances)[:, :n_songs][0])
+    
+    rec_songs = spotify_data.iloc[index]
+    rec_songs = rec_songs[~rec_songs['name'].isin(song_dict['name'])]
+    return rec_songs[metadata_cols].to_dict(orient='records')
+
+
+# In[18]:
+
+
+recommend_songs([{'name': 'Dynamite', 'year': 2020}],  data)
+
+
+# In[ ]:
+
+
+
+
